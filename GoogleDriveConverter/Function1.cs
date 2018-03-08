2f17e1a70bc810e956d379ddd4a3568027cfed07
Jeff Hollan <jehollan@microsoft.com>
2018-03-07 21:00:44 -0800

    initial

--- GoogleDriveConverter/Function1.cs
+++ GoogleDriveConverter/Function1.cs
@@ -1,34 +1,82 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json.Linq;
 
 namespace GoogleDriveConverter
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
-        {
-            log.Info("C# HTTP trigger function processed a request.");
+        private static HttpClient client = new HttpClient();
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+        [FunctionName("Convert_to_Office")]
+        public static async Task<HttpResponseMessage> Convert(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequestMessage req,
+            TraceWriter log)
+        {
+            log.Info("Convert trigger function processed a request.");
 
             // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
+            var data = await req.Content.ReadAsAsync<JObject>();
+            string filename = (string)data["name"];
 
-            // Set name to query string or body data
-            name = name ?? data?.name;
+            string mimeType;
+            switch ((string)data["type"])
+            {
+                case "application/vnd.google-apps.spreadsheet":
+                    mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    filename += ".xlsx";
+                    break;
+                case "application/vnd.google-apps.document":
+                    mimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                    filename += ".docx";
+                    break;
+                case "application/vnd.google-apps.presentation":
+                    mimeType = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                    filename += ".pptx";
+                    break;
+                default:
+                    mimeType = "@DOWNLOAD";
+                    break;
+            }
+            string url;
+            if (mimeType.Equals("@DOWNLOAD"))
+            {
+                url = $"https://content.googleapis.com/drive/v3/files/{Uri.EscapeDataString((string)data["fileId"])}?alt=media";
+            }
+            else
+            {
+                url = $"https://content.googleapis.com/drive/v3/files/{Uri.EscapeDataString((string)data["fileId"])}/export?mimeType={Uri.EscapeDataString(mimeType)}"; 
+            }
+            
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", req.Headers.Authorization.Parameter);
+            var response = await client.SendAsync(request);
+            response.Headers.Add("filename", filename);
+            return response;
+        }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        [FunctionName("List_files_in_folder")]
+        public static async Task<HttpResponseMessage> List(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequestMessage req,
+            TraceWriter log)
+        {
+            log.Info("List trigger function processed a request.");
+            // Get request body
+            var data = await req.Content.ReadAsAsync<JObject>();
+
+            string url = $"https://www.googleapis.com/drive/v3/files?q='{Uri.EscapeDataString((string)data["folderId"])}'+in+parents";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", req.Headers.Authorization.Parameter);
+            var response = await client.SendAsync(request);
+
+            return response;
         }
     }
 }
